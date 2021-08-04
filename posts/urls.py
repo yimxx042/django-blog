fdc4e45f9f0acdc58e081840975940bccb305f5c
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-04 13:05:28 -0500

    add detail page

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     #path('', views.index),
     path('posts/', views.post_list),
     #path('posts/new', views.post_create),
-    #path('posts/<int:post_id>/', views.post_detail),
+    path('posts/<int:post_id>/', views.post_detail),
     #path('posts/<int:post_id>/edit', views.post_update),
     #path('posts/<int:post_id>/delete', views.post_delete),
 ]
--- posts/views.py
+++ posts/views.py
@@ -7,3 +7,9 @@ def post_list(request):
     posts = Post.objects.all()
     context = {"posts":posts}
     return render(request, 'posts/post_list.html', context)
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context = {"post": post}
+    return render(request, 'posts/post_detail.html', context)
