c8c73875e73708e24529e4552054e1807fb85933
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-05 11:33:50 -0500

    add error

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
@@ -11,7 +11,7 @@ def post_list(request):
 
 
 def post_detail(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
     context = {"post": post}
     return render(request, 'posts/post_detail.html', context)
 
@@ -26,7 +26,7 @@ def post_create(request):
     return render(request, 'posts/post_form.html', {'form': post_form})
 
 def post_update(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
     if request.method == 'POST':
         post_form = PostForm(request.POST, instance=post)
         if post_form.is_valid():
@@ -38,13 +38,12 @@ def post_update(request, post_id):
             
 
 def post_delete(request, post_id):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
     if request.method == 'POST':
         post.delete()
         return redirect('post-list')
     else:
         return render(request, 'posts/post_confirm_delete.html', {'post': post})
-
 
 
 def index(request):
