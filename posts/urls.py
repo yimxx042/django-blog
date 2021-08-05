c9a6b281f742b2291504702ede24c23e4ddf9b9c
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-05 15:48:07 -0500

    clean code

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexRedirectView.as_view(), name='index'),
     path('posts/', views.PostListView.as_view(), name='post-list'),
     path('posts/new', views.PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,21 @@
 from django.core.exceptions import NON_FIELD_ERRORS
-from django.shortcuts import render, redirect, get_object_or_404
-from django.core.paginator import Paginator
-from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import (
+    CreateView, ListView, DetailView, UpdateView, DeleteView, RedirectView
+)
 from django.urls import reverse
 from .models import Post
 from .forms import PostForm
 
+
+
 # Create your views here.
+
+# def index(request):
+#     return redirect('post-list')
+
+class IndexRedirectView(RedirectView):
+    pattern_name = 'post-list'
+
 
 # def post_list(request):
 #     posts = Post.objects.all() #bring all data
@@ -94,7 +103,4 @@ class PostDeleteView(DeleteView):
     def get_success_url(self): 
         return reverse('post-list')
 
-        
 
-def index(request):
-    return redirect('post-list')
