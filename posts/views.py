87b5e20bab43a9c5d8fc1c95380fd1e1a9bfb921
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-05 14:29:45 -0500

    update generic view for update

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import NON_FIELD_ERRORS
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from django.urls import reverse
 from .models import Post
 from .forms import PostForm
@@ -27,10 +27,17 @@ class PostListView(ListView):
 
 
 
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    context = {"post": post}
-    return render(request, 'posts/post_detail.html', context)
+# def post_detail(request, post_id):
+#     post = get_object_or_404(Post, id=post_id)
+#     context = {"post": post}
+#     return render(request, 'posts/post_detail.html', context)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'posts/post_detail.html'
+    pk_url_kwarg = 'post_id'
+    context_object_name = 'post'
+
 
 # class PostCreateView(View):
 #     def get(self, request):
@@ -52,17 +59,27 @@ class PostCreateView(CreateView):
     def get_success_url(self):
         return reverse('post-detail', kwargs={'post_id': self.object.id})
 
-def post_update(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.method == 'POST':
-        post_form = PostForm(request.POST, instance=post)
-        if post_form.is_valid():
-            post_form.save()
-            return redirect('post-detail', post_id=post.id)
-    else:
-        post_form = PostForm(instance=post)
-    return render(request, 'posts/post_form.html', {'form': post_form})
-            
+# def post_update(request, post_id):
+#     post = get_object_or_404(Post, id=post_id)
+#     if request.method == 'POST':
+#         post_form = PostForm(request.POST, instance=post)
+#         if post_form.is_valid():
+#             post_form.save()
+#             return redirect('post-detail', post_id=post.id)
+#     else:
+#         post_form = PostForm(instance=post)
+#     return render(request, 'posts/post_form.html', {'form': post_form})
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+    pk_url_kwarg = 'post_id'
+
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'post_id':self.object.id})
+        
 
 def post_delete(request, post_id):
     post = get_object_or_404(Post, id=post_id)
