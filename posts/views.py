14a9a8b94841dbc5cecce363f76dd3e4ea861d03
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-05 13:59:26 -0500

    add generic view

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.core.exceptions import NON_FIELD_ERRORS
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
-from django.views import View
+from django.views.generic import CreateView
+from django.urls import reverse
 from .models import Post
 from .forms import PostForm
 
@@ -23,17 +24,25 @@ def post_detail(request, post_id):
     context = {"post": post}
     return render(request, 'posts/post_detail.html', context)
 
-class PostCreateView(View):
-    def get(self, request):
-        post_form = PostForm()
-        return render(request, 'posts/post_form.html', {'form': post_form})
+# class PostCreateView(View):
+#     def get(self, request):
+#         post_form = PostForm()
+#         return render(request, 'posts/post_form.html', {'form': post_form})
     
-    def post(self, request):
-        post_form = PostForm(request.POST)
-        if post_form.is_valid():
-            new_post = post_form.save()
-            return redirect('post-detail', post_id=new_post.id)
+#     def post(self, request):
+#         post_form = PostForm(request.POST)
+#         if post_form.is_valid():
+#             new_post = post_form.save()
+#             return redirect('post-detail', post_id=new_post.id)
 
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm 
+    template_name = 'posts/post_form.html'
+
+
+    def get_success_url(self):
+        return reverse('post-detail', kwargs={'post_id': self.object.id})
 
 def post_update(request, post_id):
     post = get_object_or_404(Post, id=post_id)
