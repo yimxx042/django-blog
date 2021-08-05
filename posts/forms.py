7a60659a6986f7317d8186cb4165851f93bb6334
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-05 10:04:03 -0500

    add validation *

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,19 @@
+from posts.validators import validate_symbols
 from django import forms  
 from .models import Post
+from .validators import validate_symbols
+from django.core.exceptions import ValidationError
 
 class PostForm(forms.ModelForm):
-    
+ 
     class Meta:
         model = Post
         fields = ['title', 'content']   # if wants every form them '__all__'
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if '*' in title:
+            raise ValidationError('you can`t include the *.')
+        return title 
+
+
